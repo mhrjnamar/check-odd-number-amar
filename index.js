1bcf48578c9fa6440dff4f5d33d18acee997f185
Amar Maharjan <amarm631@gmail.com>
2022-04-19 18:37:41 +0545

    responsive design code addexd

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import { Dimensions } from "react-native";
+
 export const isOdd = (num) => num%2==0? false: true; 
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+export const ph = (percent) => {
+    return (percent * screenHeight) / 100;
+  };
+  
+  export const pw = (percent) => {
+    return (percent * screenWidth) / 100;
+  };
+  
